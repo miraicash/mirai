95afd3d102e9dc0e5dac5fe2a4ad2d639b400df1
lgallagh <lucas.r.g@live.com>
2021-11-09 11:23:16 -0500

    added enpoints routes

---
+++ src/services/endpoints.js
@@ -0,0 +1,9 @@
+export const getAllUsers = `https://localhost:8000/`
+
+export const getUser = (userId) => `https://localhost:8000/${userId}`
+
+export const newUser = `https://localhost:8000/newUser`
+
+export const deleteUser = (userId) => `https://localhost:8000/${userId}`
+
+
