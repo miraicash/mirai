2c7b8bd971d0564962c49547b79e6b7984eb763a
tblandino725 <67753203+tblandino725@users.noreply.github.com>
2021-11-28 10:40:44 -0500

    Delete src directory

--- src/services/endpoints.js
+++
@@ -1,9 +0,0 @@
-export const getAllUsers = `https://localhost:8000/`
-
-export const getUser = (userId) => `https://localhost:8000/${userId}`
-
-export const newUser = `https://localhost:8000/newUser`
-
-export const deleteUser = (userId) => `https://localhost:8000/${userId}`
-
-
